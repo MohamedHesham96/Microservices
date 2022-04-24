f061242fcbe01901c32af0b15c1b21ee40371606
Mohamed Hesham <mohamed.hesham@emeint.net>
2022-04-24 03:05:24 +0200

    Initial commit

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -0,0 +1,17 @@
+package com.amigoscode.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.amigoscode.customer",
+        }
+)
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
